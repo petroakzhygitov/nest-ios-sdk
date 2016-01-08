b0f2d2726128662845a8509cf162437949a83bc1
petroakzhygitov <petro.akzhygitov@gmail.com>
2016-01-08 12:59:57 +0200

    Public headers added to NestSDK.h

--- NestSDK/NestSDK/NestSDK.h
+++ NestSDK/NestSDK/NestSDK.h
@@ -1,19 +1,28 @@
+// Copyright (c) 2016 Petro Akzhygitov <petro.akzhygitov@gmail.com>
 //
-//  NestSDK.h
-//  NestSDK
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
 //
-//  Created by Petro Akzhygitov on 7/01/16.
-//  Copyright © 2016 Petro Akzhygitov. All rights reserved.
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
 //
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
 #import <UIKit/UIKit.h>
 
-//! Project version number for NestSDK.
-FOUNDATION_EXPORT double NestSDKVersionNumber;
+#import "NestSDKApplicationDelegate.h"
+#import "NestSDKAccessToken.h"
 
-//! Project version string for NestSDK.
-FOUNDATION_EXPORT const unsigned char NestSDKVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <NestSDK/PublicHeader.h>
+#define NESTSDK_VERSION_STRING @"0.1.0"
 
 

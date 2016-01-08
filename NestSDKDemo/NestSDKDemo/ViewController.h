b0fc3b216a2f0dd685c0543bdd884204d65b9b48
petroakzhygitov <petro.akzhygitov@gmail.com>
2016-01-08 13:00:52 +0200

    Custom button added to demo project

--- NestSDKDemo/NestSDKDemo/ViewController.h
+++ NestSDKDemo/NestSDKDemo/ViewController.h
@@ -10,6 +10,7 @@
 
 @interface ViewController : UIViewController
 
+//@property (weak, nonatomic) IBOutlet NestSDKConnectWithNestButton *connectWithNestButton;
 
 @end
 
--- NestSDKDemo/NestSDKDemo/ViewController.m
+++ NestSDKDemo/NestSDKDemo/ViewController.m
@@ -6,6 +6,8 @@
 //  Copyright © 2016 Petro Akzhygitov. All rights reserved.
 //
 
+#import <NestSDK/NestSDKAuthorizationManager.h>
+#import <NestSDK/NestSDKAuthorizationManagerAuthorizationResult.h>
 #import "ViewController.h"
 
 @interface ViewController ()
@@ -16,12 +18,53 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    // Connect with Nest using NestSDKConnectWithNestButton
+//    [self createConnectWithNestButton];
+
+    // Connect with Nest using your custom button
+    [self createCustomConnectWithNestButton];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)createConnectWithNestButton {
+//    NestSDKConnectWithNestButton *connectWithNestButton = [[NestSDKConnectWithNestButton alloc] init];
+//    // Optional: Place the button in the center of your view.
+//    connectWithNestButton.center = self.view.center;
+//
+//    [self.view addSubview:connectWithNestButton];
+}
+
+- (void)createCustomConnectWithNestButton {
+    // Add a custom login button to your app
+    UIButton *customConnectWithNestButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    customConnectWithNestButton.backgroundColor = [UIColor darkGrayColor];
+    customConnectWithNestButton.frame = CGRectMake(0, 0, 240, 40);
+
+    // Optional: Place the button in the center of your view.
+    customConnectWithNestButton.center = self.view.center;
+    [customConnectWithNestButton setTitle:@"Custom Connect Button" forState:UIControlStateNormal];
+
+    // Handle clicks on the button
+    [customConnectWithNestButton addTarget:self action:@selector(customConnectWithNestButtonClicked)
+                          forControlEvents:UIControlEventTouchUpInside];
+
+    // Add the button to the view
+    [self.view addSubview:customConnectWithNestButton];
+}
+
+// Once the button is clicked, show the auth dialog
+- (void)customConnectWithNestButtonClicked {
+    NestSDKAuthorizationManager *authorizationManager = [[NestSDKAuthorizationManager alloc] init];
+    [authorizationManager authorizeWithNestAccountFromViewController:self
+                                                             handler:^(NestSDKAuthorizationManagerAuthorizationResult *result, NSError *error) {
+                                                                 if (error) {
+                                                                     NSLog(@"Process error: %@", error);
+                                                                 } else if (result.isCancelled) {
+                                                                     NSLog(@"Cancelled");
+                                                                 } else {
+                                                                     NSLog(@"Authorized!");
+                                                                 }
+                                                             }];
 }
 
 @end

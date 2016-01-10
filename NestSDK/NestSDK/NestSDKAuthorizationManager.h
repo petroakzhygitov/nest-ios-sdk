f562fa0427ed6e90c12b136821ad561c8b94a02a
petroakzhygitov <petro.akzhygitov@gmail.com>
2016-01-11 01:34:22 +0200

    Authorization manager refactored and cleaned up

--- NestSDK/NestSDK/NestSDKAuthorizationManager.h
+++ NestSDK/NestSDK/NestSDKAuthorizationManager.h
@@ -1,29 +1,70 @@
+// Copyright (c) 2016 Petro Akzhygitov <petro.akzhygitov@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 #import "NestSDKAuthorizationViewController.h"
 
 @class NestSDKAuthorizationManagerAuthorizationResult;
 
-#pragma mark macros
-
-#pragma mark const
-
-#pragma mark enum
-
 #pragma mark typedef
 
+/**
+ * Describes the call back to the NestSDKAuthorizationManager
+ *
+ * @param result the result of the authorization
+ * @param error the authorization error, if any.
+ */
 typedef void (^NestSDKAuthorizationManagerAuthorizationHandler)(NestSDKAuthorizationManagerAuthorizationResult *result, NSError *error);
 
-#pragma mark Protocol
-
+/**
+ * NestSDKAuthorizationManager provides methods for authorizing and unauthorizing the user
+ * NestSDKAuthorizationManager works directly with [NestSDKAccessToken currentAccessToken]
+ * and sets the currentAccessToken upon successful authorizations (or sets nil in case of unauthorization).
+ *
+ * You should check [NestSDKAccessToken currentAccessToken] before calling authorize* to see if there is
+ * a cached token available (typically in your viewDidLoad).
+ *
+ * If you are managing your own token instances outside of currentAccessToken, you will need to set
+ * currentAccessToken before calling authorize*.
+ */
 @interface NestSDKAuthorizationManager : NSObject <NestSDKAuthorizationViewControllerDelegate>
-#pragma mark Properties
-
 #pragma mark Methods
 
+/**
+ * Authorizes the user.
+ *
+ * This method will present UI the user. You typically should check if [NestSDKAccessToken currentAccessToken]
+ * before calling this method. For example, you could make that check at viewDidLoad.
+ *
+ * @param the view controller to present from.
+ * @param handler the callback.
+ */
 - (void)authorizeWithNestAccountFromViewController:(UIViewController *)viewController
                                            handler:(NestSDKAuthorizationManagerAuthorizationHandler)handler;
 
+/**
+ * Unauthorizes the user.
+ *
+ * This calls [NestSDKAccessToken setCurrentAccessToken:nil]
+ */
 - (void)unauthorize;
 
 @end
--- NestSDK/NestSDK/NestSDKAuthorizationManager.m
+++ NestSDK/NestSDK/NestSDKAuthorizationManager.m
@@ -1,9 +1,28 @@
+// Copyright (c) 2016 Petro Akzhygitov <petro.akzhygitov@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 #import <libkern/OSAtomic.h>
 #import "NestSDKAuthorizationManager.h"
 #import "NestSDKAuthorizationManagerAuthorizationResult.h"
 #import "NestSDKAccessToken.h"
-
-#pragma mark macros
+#import "NestSDKError.h"
 
 #pragma mark const
 
@@ -11,118 +30,164 @@ static NSString *const kNestOAuthDomainString = @"home.nest.com";
 
 static NSString *const kNestProductIDKey = @"NestProductID";
 static NSString *const kNestProductSecretKey = @"NestProductSecret";
-static NSString *const kNestStateKey = @"NestState";
 static NSString *const kNestRedirectURLKey = @"NestRedirectURL";
 
 static NSString *const kNestAuthorizationURLFormat = @"https://%@/login/oauth2?client_id=%@&state=%@";
 static NSString *const kNestAccessTokenURLFormat = @"https://api.%@/oauth2/access_token?code=%@&client_id=%@&client_secret=%@&grant_type=authorization_code";
 
-#pragma mark enum
+static NSString *const kJSONKeyAccessToken = @"access_token";
+static NSString *const kJSONKeyExpiresIn = @"expires_in";
 
-#pragma mark typedef
+static NSString *const KHTTPHeaderKeyContentType = @"Content-Type";
+static NSString *const kHeaderValueFormData = @"form-data";
+
+static NSString *const kHTTPMethodPost = @"POST";
+
+static const Byte kHTTPStatusCodeOK = 200;
+static const NSTimeInterval kDefaultTimeoutInterval = 8.0;
 
 
-@implementation NestSDKAuthorizationManager {
-#pragma mark Instance variables
-    NestSDKAuthorizationViewController *_authorizationViewController;
-    NestSDKAuthorizationManagerAuthorizationHandler _handler;
+@interface NestSDKAuthorizationManager ()
 
-    int _authorizing;
-}
+@property(nonatomic) NestSDKAuthorizationViewController *authorizationViewController;
+@property(nonatomic, copy) NestSDKAuthorizationManagerAuthorizationHandler handler;
 
-#pragma mark Initializer
+@end
 
+
+@implementation NestSDKAuthorizationManager
 #pragma mark Private
 
-- (NSString *)p_clientID {
+- (NSString *)_state {
+    // Each request generate unique state string based on bundle identifier of the app and UUID string
+    NSString *bundleIdentifier = [[NSBundle mainBundle] bundleIdentifier];
+    return [bundleIdentifier stringByAppendingFormat:@"_%@", [[NSUUID UUID] UUIDString]];
+}
+
+- (NSString *)_productID {
     return [[NSBundle mainBundle] objectForInfoDictionaryKey:kNestProductIDKey];
 }
 
-- (NSString *)p_clientSecret {
+- (NSString *)_productSecret {
     return [[NSBundle mainBundle] objectForInfoDictionaryKey:kNestProductSecretKey];
 }
 
-- (NSString *)p_state {
-    return [[NSBundle mainBundle] objectForInfoDictionaryKey:kNestStateKey];
-}
-
-- (NSURL *)p_redirectURL {
+- (NSURL *)_redirectURL {
     NSString *urlString = [[NSBundle mainBundle] objectForInfoDictionaryKey:kNestRedirectURLKey];
     return [NSURL URLWithString:urlString];
 }
 
-- (NSURL *)p_authorizationURL {
-    NSString *urlString = [NSString stringWithFormat:kNestAuthorizationURLFormat, kNestOAuthDomainString, [self p_clientID], [self p_state]];
+- (NSURL *)_authorizationURL {
+    NSString *urlString = [NSString stringWithFormat:kNestAuthorizationURLFormat, kNestOAuthDomainString, [self _productID], [self _state]];
     return [NSURL URLWithString:urlString];
 }
 
-- (NSURL *)p_accessTokenURLWithAuthorizationCode:(NSString *)authorizationCode {
-    NSString *urlString = [NSString stringWithFormat:kNestAccessTokenURLFormat, kNestOAuthDomainString, authorizationCode, [self p_clientID], [self p_clientSecret]];
+- (NSURL *)_accessTokenURLWithAuthorizationCode:(NSString *)authorizationCode {
+    NSString *urlString = [NSString stringWithFormat:kNestAccessTokenURLFormat, kNestOAuthDomainString, authorizationCode, [self _productID], [self _productSecret]];
     return [NSURL URLWithString:urlString];
 }
 
-- (NestSDKAuthorizationManagerAuthorizationResult *)p_authorizationResultWithData:(NSData *)data {
-    NSError *error;
-    NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:0 error:&error];
+- (NestSDKAuthorizationManagerAuthorizationResult *)_authorizationResultWithData:(NSData *)data error:(NSError **)error {
+    NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:0 error:error];
+    if (*error) return nil;
 
-    NSString *tokenString = json[@"access_token"];
-    NSDate *expirationDate = [[NSDate date] dateByAddingTimeInterval:[json[@"expires_in"] longValue]];
+    NSString *tokenString = json[kJSONKeyAccessToken];
+    NSDate *expirationDate = [[NSDate date] dateByAddingTimeInterval:[json[kJSONKeyExpiresIn] longValue]];
 
     NestSDKAccessToken *accessToken = [[NestSDKAccessToken alloc] initWithTokenString:tokenString expirationDate:expirationDate];
 
     return [[NestSDKAuthorizationManagerAuthorizationResult alloc] initWithToken:accessToken isCancelled:NO];
 }
 
-- (void)p_obtainTokenWithAuthorizationCode:(NSString *)authorizationCode {
-    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:[self p_accessTokenURLWithAuthorizationCode:authorizationCode]];
-    [request setHTTPMethod:@"POST"];
-    [request setValue:@"form-data" forHTTPHeaderField:@"Content-Type"];
+- (void)_obtainTokenWithAuthorizationCode:(NSString *)authorizationCode {
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:[self _accessTokenURLWithAuthorizationCode:authorizationCode]];
+    [request setHTTPMethod:kHTTPMethodPost];
+    [request setValue:kHeaderValueFormData forHTTPHeaderField:KHTTPHeaderKeyContentType];
 
     NSURLSessionConfiguration *urlSessionConfiguration = [NSURLSessionConfiguration defaultSessionConfiguration];
     urlSessionConfiguration.allowsCellularAccess = YES;
-    urlSessionConfiguration.timeoutIntervalForRequest = 8.0;
-    urlSessionConfiguration.timeoutIntervalForResource = 8.0;
+    urlSessionConfiguration.timeoutIntervalForRequest = kDefaultTimeoutInterval;
+    urlSessionConfiguration.timeoutIntervalForResource = kDefaultTimeoutInterval;
 
     NSURLSession *urlSession = [NSURLSession sessionWithConfiguration:urlSessionConfiguration delegate:nil delegateQueue:nil];
 
     NSURLSessionDataTask *dataTask = [urlSession dataTaskWithRequest:request
                                                    completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
                                                        if (error) {
-                                                           [self p_finishAuthorizationWithAuthorizationResult:nil error:error];
+                                                           [self _finishAuthorizationWithAuthorizationResult:nil error:error];
 
-                                                       } else {
-                                                           NestSDKAuthorizationManagerAuthorizationResult *authorizationResult = [self p_authorizationResultWithData:data];
-                                                           [NestSDKAccessToken setCurrentAccessToken:authorizationResult.token];
-
-                                                           [self p_finishAuthorizationWithAuthorizationResult:authorizationResult error:nil];
+                                                           return;
                                                        }
+
+                                                       NSInteger responseStatusCode = ((NSHTTPURLResponse *) response).statusCode;
+                                                       if (responseStatusCode != kHTTPStatusCodeOK) {
+                                                           NSError *badResponseError = [NestSDKError badResponseErrorWithStatusCode:responseStatusCode message:nil];
+                                                           [self _finishAuthorizationWithAuthorizationResult:nil error:badResponseError];
+
+                                                           return;
+                                                       }
+
+                                                       NestSDKAuthorizationManagerAuthorizationResult *authorizationResult = [self _authorizationResultWithData:data error:&error];
+                                                       if (error) {
+                                                           [self _finishAuthorizationWithAuthorizationResult:nil error:error];
+
+                                                           return;
+                                                       }
+
+                                                       [NestSDKAccessToken setCurrentAccessToken:authorizationResult.token];
+
+                                                       [self _finishAuthorizationWithAuthorizationResult:authorizationResult error:nil];
                                                    }];
     [dataTask resume];
 }
 
-- (void)p_finishAuthorizationWithAuthorizationResult:(NestSDKAuthorizationManagerAuthorizationResult *)authorizationResult error:(NSError *)error {
-    if (!OSAtomicCompareAndSwapInt(YES, NO, &_authorizing)) return;
+- (void)_finishAuthorizationWithAuthorizationResult:(NestSDKAuthorizationManagerAuthorizationResult *)authorizationResult error:(NSError *)error {
+    self.handler(authorizationResult, error);
+    self.handler = nil;
 
-    _handler(authorizationResult, error);
-    [_authorizationViewController dismissViewControllerAnimated:YES completion:nil];
+    [self.authorizationViewController dismissViewControllerAnimated:YES completion:nil];
+    self.authorizationViewController = nil;
 }
-
-#pragma mark Notification selectors
-
-#pragma mark Override
 
 #pragma mark Public
 
 - (void)authorizeWithNestAccountFromViewController:(UIViewController *)viewController
                                            handler:(NestSDKAuthorizationManagerAuthorizationHandler)handler {
     if (!handler) return;
+    
+    self.handler = [handler copy];
+    
+    if (!viewController) {
+        NSError *error = [NestSDKError argumentRequiredErrorWithName:@"viewController" message:nil];
+        [self _finishAuthorizationWithAuthorizationResult:nil error:error];
+        
+        return;
+    }
 
-    if (!OSAtomicCompareAndSwapInt(NO, YES, &_authorizing)) return;
+    if ([self _productID].length == 0) {
+        NSError *error = [NestSDKError mainBundleKeyRequiredErrorWithKey:kNestProductIDKey message:nil];
+        [self _finishAuthorizationWithAuthorizationResult:nil error:error];
 
-    _handler = [handler copy];
-    _authorizationViewController = [[NestSDKAuthorizationViewController alloc] initWithAuthorizationURL:[self p_authorizationURL]
-                                                                                            redirectURL:[self p_redirectURL]
-                                                                                               delegate:self];
+        return;
+    }
+
+    if ([self _productSecret].length == 0) {
+        NSError *error = [NestSDKError mainBundleKeyRequiredErrorWithKey:kNestProductSecretKey message:nil];
+        [self _finishAuthorizationWithAuthorizationResult:nil error:error];
+
+        return;
+    }
+
+    if ([self _redirectURL].absoluteString.length == 0) {
+        NSError *error = [NestSDKError mainBundleKeyRequiredErrorWithKey:kNestRedirectURLKey message:nil];
+        [self _finishAuthorizationWithAuthorizationResult:nil error:error];
+
+        return;
+    }
+    
+    self.authorizationViewController = [[NestSDKAuthorizationViewController alloc] initWithAuthorizationURL:[self _authorizationURL]
+                                                                                                redirectURL:[self _redirectURL]
+                                                                                                   delegate:self];
 
     [viewController presentViewController:_authorizationViewController animated:YES completion:nil];
 }
@@ -131,20 +196,15 @@ static NSString *const kNestAccessTokenURLFormat = @"https://api.%@/oauth2/acces
     [NestSDKAccessToken setCurrentAccessToken:nil];
 }
 
-
-#pragma mark IBAction
-
-#pragma mark Protocol @protocol-name
-
-#pragma mark Delegate <NestSDKAuthorizationViewControllerDelegate>
+#pragma mark NestSDKAuthorizationViewControllerDelegate
 
 - (void)hasCancelledAuthorization {
     NestSDKAuthorizationManagerAuthorizationResult *result = [[NestSDKAuthorizationManagerAuthorizationResult alloc] initWithToken:nil isCancelled:YES];
-    [self p_finishAuthorizationWithAuthorizationResult:result error:nil];
+    [self _finishAuthorizationWithAuthorizationResult:result error:nil];
 }
 
 - (void)hasReceivedAuthorizationCode:(NSString *)authorizationCode {
-    [self p_obtainTokenWithAuthorizationCode:authorizationCode];
+    [self _obtainTokenWithAuthorizationCode:authorizationCode];
 }
 
 @end

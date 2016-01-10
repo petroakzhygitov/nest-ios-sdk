377a034c120ddd78ec1ad038563b30022b153db8
petroakzhygitov <petro.akzhygitov@gmail.com>
2016-01-10 16:54:52 +0200

    NestSDKApplicationDelegate docs added

--- NestSDK/NestSDK/NestSDKApplicationDelegate.h
+++ NestSDK/NestSDK/NestSDKApplicationDelegate.h
@@ -12,26 +12,45 @@
 #pragma mark Protocol
 
 /**
-
- `AFHTTPSessionManager` is a subclass of `AFURLSessionManager` with convenience methods for making HTTP requests. When a `baseURL` is provided, requests made with the `GET` / `POST` / et al. convenience methods can be made with relative paths.
- ## Subclassing Notes
- Developers targeting iOS 7 or Mac OS X 10.9 or later that deal extensively with a web service are encouraged to subclass `AFHTTPSessionManager`, providing a class method that returns a shared singleton object on which authentication and other configuration can be shared across the application.
- For developers targeting iOS 6 or Mac OS X 10.8 or earlier, `AFHTTPRequestOperationManager` may be used to similar effect.
- 
+ * NestSDKApplicationDelegate is main connection point with Nest SDK.
+ * It is designed to be able process the results from Nest authorization dialogs (native app or Safari) in the future.
  */
-
 @interface NestSDKApplicationDelegate : NSObject
 #pragma mark Properties
 
 #pragma mark Methods
 
+/**
+ * Gets the singleton instance.
+ */
 + (instancetype)sharedInstance;
 
+/**
+ * Call this method from the [UIApplicationDelegate application:openURL:sourceApplication:annotation:] method
+ * of the AppDelegate for your app. It should be invoked for the proper processing of responses during interaction
+ * with the native app or Safari as part of SSO authorization flow in the future.
+ *
+ * @param application The application as passed to [UIApplicationDelegate application:openURL:sourceApplication:annotation:].
+ * @param url The URL as passed to [UIApplicationDelegate application:openURL:sourceApplication:annotation:].
+ * @param sourceApplication The sourceApplication as passed to [UIApplicationDelegate application:openURL:sourceApplication:annotation:].
+ * @param annotation The annotation as passed to [UIApplicationDelegate application:openURL:sourceApplication:annotation:].
+ *
+ * @return NO in any case for now.
+ */
 - (BOOL)application:(UIApplication *)application
             openURL:(NSURL *)url
   sourceApplication:(NSString *)sourceApplication
          annotation:(id)annotation;
 
+/**
+ * Call this method from the [UIApplicationDelegate application:didFinishLaunchingWithOptions:] method
+ * of the AppDelegate for your app. It should be invoked for the proper use of the Nest SDK in the future.
+ *
+ * @param application The application as passed to [UIApplicationDelegate application:didFinishLaunchingWithOptions:].
+ * @param launchOptions The launchOptions as passed to [UIApplicationDelegate application:didFinishLaunchingWithOptions:].
+ *
+ * @return NO in any case for now.
+ */
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions;
 
 @end

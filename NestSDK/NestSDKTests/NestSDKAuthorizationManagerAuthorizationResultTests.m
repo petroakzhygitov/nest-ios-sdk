062f55e8b244d8a490bae2ff2e543e518351d272
petroakzhygitov <petro.akzhygitov@gmail.com>
2016-01-11 10:25:07 +0200

    Authorization result test added

---
+++ NestSDK/NestSDKTests/NestSDKAuthorizationManagerAuthorizationResultTests.m
@@ -0,0 +1,43 @@
+// Copyright (c) 2016 Petro Akzhygitov <petro.akzhygitov@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#import <OCMock/OCMock.h>
+#import "SpectaDSL.h"
+#import "SPTSpec.h"
+#import "Expecta.h"
+#import "NestSDKAccessToken.h"
+#import "NestSDKAuthorizationManagerAuthorizationResult.h"
+#import "LSStubRequestDSL.h"
+#import "LSNocilla.h"
+
+SpecBegin(NestSDKAuthorizationManagerAuthorizationResult)
+    {
+        describe(@"NestSDKAuthorizationManagerAuthorizationResult", ^{
+
+            it(@"should init and set properties", ^{
+                NestSDKAccessToken *accessToken = [[NestSDKAccessToken alloc] initWithTokenString:@"qwerty" expirationDate:[NSDate date]];
+                NestSDKAuthorizationManagerAuthorizationResult *authorizationResult = [[NestSDKAuthorizationManagerAuthorizationResult alloc] initWithToken:accessToken
+                                                                                                                                                isCancelled:YES];
+                expect([authorizationResult.token isEqualToAccessToken:accessToken]).to.equal(YES);
+                expect(authorizationResult.isCancelled).to.equal(YES);
+            });
+        });
+    }
+SpecEnd

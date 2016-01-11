8834f1595c3ce92f4a431c4e3063798c12bd82d6
petroakzhygitov <petro.akzhygitov@gmail.com>
2016-01-11 10:25:28 +0200

    Authorization result refactored and cleaned up

--- NestSDK/NestSDK/NestSDKAuthorizationManagerAuthorizationResult.m
+++ NestSDK/NestSDK/NestSDKAuthorizationManagerAuthorizationResult.m
@@ -1,26 +1,43 @@
+// Copyright (c) 2016 Petro Akzhygitov <petro.akzhygitov@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 #import "NestSDKAuthorizationManagerAuthorizationResult.h"
 #import "NestSDKAccessToken.h"
 
-#pragma mark macros
+@interface NestSDKAuthorizationManagerAuthorizationResult ()
+#pragma mark Properties
 
-#pragma mark const
+@property(readwrite, copy, nonatomic) NestSDKAccessToken *token;
+@property(readwrite, nonatomic) BOOL isCancelled;
 
-#pragma mark enum
-
-#pragma mark typedef
+@end
 
 
-@implementation NestSDKAuthorizationManagerAuthorizationResult {
-#pragma mark Instance variables
-}
-
+@implementation NestSDKAuthorizationManagerAuthorizationResult
 #pragma mark Initializer
 
 - (instancetype)initWithToken:(NestSDKAccessToken *)token isCancelled:(BOOL)isCancelled {
     self = [super init];
     if (self) {
-        _token = token;
-        _isCancelled = isCancelled;
+        self.token = token;
+        self.isCancelled = isCancelled;
     }
 
     return self;
@@ -29,19 +46,5 @@
 - (instancetype)init {
     return [self initWithToken:nil isCancelled:NO];
 }
-
-#pragma mark Private
-
-#pragma mark Notification selectors
-
-#pragma mark Override
-
-#pragma mark Public
-
-#pragma mark IBAction
-
-#pragma mark Protocol @protocol-name
-
-#pragma mark Delegate @delegate-name
 
 @end

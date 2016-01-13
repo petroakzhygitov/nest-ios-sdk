c0d8efbe253f217c6e3b24b89f8e235a66ab4cec
petroakzhygitov <petro.akzhygitov@gmail.com>
2016-01-13 19:07:23 +0200

    Device data model refactored and cleaned up

--- NestSDK/NestSDK/NestSDKDevice.h
+++ NestSDK/NestSDK/NestSDKDevice.h
@@ -1,36 +1,86 @@
+// Copyright (c) 2016 Petro Akzhygitov <petro.akzhygitov@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 #import <Foundation/Foundation.h>
-#import "JSONModel.h"
+#import <JSONModel/JSONModel.h>
+#import <NestSDK/NestSDKDataModel.h>
 
-#pragma mark macros
-
-#pragma mark const
-
-#pragma mark enum
-
-#pragma mark typedef
-
-#pragma mark Protocol
-
-@protocol NestSDKDevice
-
-@end
-
-
-@interface NestSDKDevice : JSONModel <NestSDKDevice>
+/**
+ * Base class for Nest devices.
+ */
+@interface NestSDKDevice : NestSDKDataModel
 #pragma mark Properties
 
-@property(nonatomic, copy) NSString <Optional> *device_id;
+/**
+ * Device unique identifier.
+ */
+@property(nonatomic, copy) NSString <Optional> *deviceId;
+
+/**
+ * Specifies language and region (or country) preference.
+ */
 @property(nonatomic, copy) NSString <Optional> *locale;
-@property(nonatomic, copy) NSString <Optional> *software_version;
-@property(nonatomic, copy) NSString <Optional> *structure_id;
+
+/**
+ * Software version.
+ */
+@property(nonatomic, copy) NSString <Optional> *softwareVersion;
+
+/**
+ * Structure unique identifier.
+ */
+@property(nonatomic, copy) NSString <Optional> *structureId;
+
+/**
+ * Display name of the device. This data value is presented in labels in the Nest app (https://home.nest.com/).
+ *
+ * Learn more about where names for
+ *      Nest Thermostats: https://nest.com/support/article/How-do-I-change-the-name-of-my-Nest-Learning-Thermostat
+ *      Nest Protects: https://nest.com/support/article/Learn-more-about-Nest-Protect-locations-names-and-labels
+ *      Nest Cams: https://nest.com/support/article/How-do-I-change-the-name-of-my-Nest-Cam
+ */
 @property(nonatomic, copy) NSString <Optional> *name;
-@property(nonatomic, copy) NSString <Optional> *name_long;
-@property(nonatomic, copy) NSString <Optional> *last_connection;
 
-@property(nonatomic) BOOL is_online;
+/**
+ * Long display name of the device. This data value is selected by the user and presented in labels in the Nest app.
+ * Similar to name.
+* Learn more about where names for
+ *      Nest Thermostats: https://nest.com/support/article/How-do-I-change-the-name-of-my-Nest-Learning-Thermostat
+ *      Nest Protects: https://nest.com/support/article/Learn-more-about-Nest-Protect-locations-names-and-labels
+ *      Nest Cams: https://nest.com/support/article/How-do-I-change-the-name-of-my-Nest-Cam
+ */
+@property(nonatomic, copy) NSString <Optional> *nameLong;
 
-@property(nonatomic, copy) NSString <Optional> *where_id;
+/**
+ * Timestamp of the last successful interaction with the Nest service.
+ */
+@property(nonatomic) NSDate <Optional> *lastConnection;
 
-#pragma mark Methods
+/**
+ * Device connection status with the Nest Service.
+ */
+@property(nonatomic) BOOL isOnline;
+
+/**
+ * whereId is a unique, Nest-generated identifier that represents name, the display name of the device.
+ */
+@property(nonatomic, copy) NSString <Optional> *whereId;
 
 @end
--- NestSDK/NestSDK/NestSDKDevice.m
+++ NestSDK/NestSDK/NestSDKDevice.m
@@ -1,32 +1,73 @@
+// Copyright (c) 2016 Petro Akzhygitov <petro.akzhygitov@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 #import "NestSDKDevice.h"
+#import "NestSDKUtils.h"
 
-#pragma mark macros
-
-#pragma mark const
-
-#pragma mark enum
-
-#pragma mark typedef
-
-
-@implementation NestSDKDevice {
-#pragma mark Instance variables
-}
-
-#pragma mark Initializer
-
-#pragma mark Private
-
-#pragma mark Notification selectors
-
+@implementation NestSDKDevice
 #pragma mark Override
 
-#pragma mark Public
+- (void)setLastConnectionWithNSString:(NSString *)peakPeriodStartTimeString {
+    self.lastConnection = [NestSDKUtils dateWithISO8601FormatDateString:peakPeriodStartTimeString];
+}
 
-#pragma mark IBAction
+- (id)JSONObjectForLastConnection {
+    return [NestSDKUtils iso8601FormatDateStringWithDate:self.lastConnection];
+}
 
-#pragma mark Protocol @protocol-name
+- (NSUInteger)hash {
+    NSUInteger intValueForYes = 1231;
+    NSUInteger intValueForNo = 1237;
 
-#pragma mark Delegate @delegate-name
+    NSUInteger prime = 31;
+    NSUInteger result = 1;
 
+    result = prime * result + self.deviceId.hash;
+    result = prime * result + self.locale.hash;
+    result = prime * result + self.softwareVersion.hash;
+    result = prime * result + self.structureId.hash;
+    result = prime * result + self.name.hash;
+    result = prime * result + self.nameLong.hash;
+    result = prime * result + self.lastConnection.hash;
+    result = prime * result + (self.isOnline ? intValueForYes : intValueForNo);
+    result = prime * result + self.whereId.hash;
+
+    return result;
+}
+
+- (BOOL)isEqual:(id)other {
+    if (other == self)
+        return YES;
+
+    if (!other || ![[other class] isEqual:[self class]])
+        return NO;
+
+    NestSDKDevice *otherDevice = (NestSDKDevice *) other;
+    return ([NestSDKUtils object:self.deviceId isEqualToObject:otherDevice.deviceId] &&
+            ([NestSDKUtils object:self.locale isEqualToObject:otherDevice.locale]) &&
+            ([NestSDKUtils object:self.softwareVersion isEqualToObject:otherDevice.softwareVersion]) &&
+            ([NestSDKUtils object:self.structureId isEqualToObject:otherDevice.structureId]) &&
+            ([NestSDKUtils object:self.name isEqualToObject:otherDevice.name]) &&
+            ([NestSDKUtils object:self.nameLong isEqualToObject:otherDevice.nameLong]) &&
+            ([NestSDKUtils object:self.lastConnection isEqualToObject:otherDevice.lastConnection]) &&
+            (self.isOnline == otherDevice.isOnline) &&
+            ([NestSDKUtils object:self.whereId isEqualToObject:otherDevice.whereId]));
+}
 @end

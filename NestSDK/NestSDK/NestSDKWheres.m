e19fb4d44d41a97458d881794e5757a6da97dadc
petroakzhygitov <petro.akzhygitov@gmail.com>
2016-01-13 12:27:41 +0200

    Wheres hash and equal methods added

--- NestSDK/NestSDK/NestSDKWheres.m
+++ NestSDK/NestSDK/NestSDKWheres.m
@@ -4,5 +4,27 @@
 
 @implementation NestSDKWheres
 
+- (NSUInteger)hash {
+    NSUInteger prime = 31;
+    NSUInteger result = 1;
+
+    result = prime * result + self.name.hash;
+    result = prime * result + self.whereId.hash;
+
+    return result;
+}
+
+- (BOOL)isEqual:(id)other {
+    if (other == self)
+        return YES;
+
+    if (!other || ![[other class] isEqual:[self class]])
+        return NO;
+
+    NestSDKWheres *otherWheres = (NestSDKWheres *) other;
+    return ((!self.name || [self.name isEqualToString:otherWheres.name]) &&
+            (!self.whereId || [self.whereId isEqualToString:otherWheres.whereId]));
+}
+
 
 @end

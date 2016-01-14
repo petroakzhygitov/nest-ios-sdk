31cebba3afc7f6e43c4eaca0bb9e4dd964d37991
petroakzhygitov <petro.akzhygitov@gmail.com>
2016-01-14 15:19:40 +0200

    Smoke+CO alarm data model refactored and cleaned up

--- NestSDK/NestSDK/NestSDKSmokeCOAlarm.h
+++ NestSDK/NestSDK/NestSDKSmokeCOAlarm.h
@@ -1,31 +1,99 @@
+// Copyright (c) 2016 Petro Akzhygitov <petro.akzhygitov@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 #import <Foundation/Foundation.h>
 #import <JSONModel/JSONModel.h>
 #import "NestSDKDevice.h"
 
-#pragma mark macros
-
-#pragma mark const
-
-#pragma mark enum
 
 #pragma mark typedef
+typedef NS_ENUM(NSUInteger, NestSDKSmokeCOAlarmBatteryHealth) {
+    NestSDKSmokeCOAlarmBatteryHealthUndefined = 0,
+    NestSDKSmokeCOAlarmBatteryHealthOk,
+    NestSDKSmokeCOAlarmBatteryHealthReplace
+};
 
-#pragma mark Protocol
+typedef NS_ENUM(NSUInteger, NestSDKSmokeCOAlarmAlarmState) {
+    NestSDKSmokeCOAlarmAlarmStateUndefined = 0,
+    NestSDKSmokeCOAlarmAlarmStateOk,
+    NestSDKSmokeCOAlarmAlarmStateWarning,
+    NestSDKSmokeCOAlarmAlarmStateEmergency
+};
 
+typedef NS_ENUM(NSUInteger, NestSDKSmokeCOAlarmUIColorState) {
+    NestSDKSmokeCOAlarmUIColorStateUndefined = 0,
+    NestSDKSmokeCOAlarmUIColorStateGray,
+    NestSDKSmokeCOAlarmUIColorStateGreen,
+    NestSDKSmokeCOAlarmUIColorStateYellow,
+    NestSDKSmokeCOAlarmUIColorStateRed
+};
+
+
+/**
+ * Smoke+CO alarm data model
+ */
 @interface NestSDKSmokeCOAlarm : NestSDKDevice
 #pragma mark Properties
 
-@property(nonatomic, copy) NSString <Optional> *battery_health;
+/**
+ * Specifies language and region (or country) preference.
+ */
+@property(nonatomic, copy) NSString <Optional> *locale;
 
-@property(nonatomic, copy) NSString <Optional> *co_alarm_state;
-@property(nonatomic, copy) NSString <Optional> *smoke_alarm_state;
+/**
+ * Timestamp of the last successful interaction with the Nest service.
+ */
+@property(nonatomic) NSDate <Optional> *lastConnection;
 
-@property(nonatomic) BOOL is_manual_test_active;
+/**
+ * Battery life/health; estimate of remaining battery power level.
+ */
+@property(nonatomic) NestSDKSmokeCOAlarmBatteryHealth batteryHealth;
 
-@property(nonatomic, copy) NSString <Optional> *last_manual_test_time;
+/**
+ * CO alarm status.
+ */
+@property(nonatomic) NestSDKSmokeCOAlarmAlarmState coAlarmState;
 
-@property(nonatomic, copy) NSString <Optional> *ui_color_state;
+/**
+ * Smoke alarm status.
+ */
+@property(nonatomic) NestSDKSmokeCOAlarmAlarmState smokeAlarmState;
 
-#pragma mark Methods
+/**
+ * State of the manual smoke and CO alarm test.
+ */
+@property(nonatomic) BOOL isManualTestActive;
+
+/**
+ * Timestamp of the last successful manual smoke and CO alarm test.
+ */
+@property(nonatomic) NSDate <Optional> *lastManualTestTime;
+
+/**
+ * Indicates device status by color in the Nest app UI.
+ * It is an aggregate condition for battery+smoke+co states, and reflects the actual color indicators displayed in the Nest app.
+ *
+ * Learn more about color indicators on the Nest Protect: http://support.nest.com/article/What-do-the-lights-mean-on-Nest-Protect
+ * Learn more about color indicators for the Nest Protect battery: http://support.nest.com/article/How-do-I-check-the-level-of-my-Nest-Protect-battery
+ */
+@property(nonatomic) NestSDKSmokeCOAlarmUIColorState uiColorState;
 
 @end
--- NestSDK/NestSDK/NestSDKSmokeCOAlarm.m
+++ NestSDK/NestSDK/NestSDKSmokeCOAlarm.m
@@ -1,37 +1,89 @@
+// Copyright (c) 2016 Petro Akzhygitov <petro.akzhygitov@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 #import "NestSDKSmokeCOAlarm.h"
-
-#pragma mark macros
-
-#pragma mark const
-
-#pragma mark enum
-
-#pragma mark typedef
+#import "NestSDKUtils.h"
 
 
-@implementation NestSDKSmokeCOAlarm {
-#pragma mark Instance variables
-}
-
-#pragma mark Initializer
-
-#pragma mark Private
-
-#pragma mark Notification selectors
-
+@implementation NestSDKSmokeCOAlarm
 #pragma mark Override
 
-+ (BOOL)propertyIsOptional:(NSString *)propertyName {
-    // All primitives are optional
-    return YES;
+- (void)setLastConnectionWithNSString:(NSString *)peakPeriodStartTimeString {
+    self.lastConnection = [NestSDKUtils dateWithISO8601FormatDateString:peakPeriodStartTimeString];
 }
 
-#pragma mark Public
+- (id)JSONObjectForLastConnection {
+    return [NestSDKUtils iso8601FormatDateStringWithDate:self.lastConnection];
+}
 
-#pragma mark IBAction
+- (void)setLastManualTestTimeWithNSString:(NSString *)lastManualTestTimeString {
+    self.lastManualTestTime = [NestSDKUtils dateWithISO8601FormatDateString:lastManualTestTimeString];
+}
 
-#pragma mark Protocol @protocol-name
+- (id)JSONObjectForLastManualTestTime {
+    return [NestSDKUtils iso8601FormatDateStringWithDate:self.lastManualTestTime];
+}
 
-#pragma mark Delegate @delegate-name
+
+- (NSUInteger)hash {
+    NSUInteger intValueForYes = 1231;
+    NSUInteger intValueForNo = 1237;
+
+    NSUInteger prime = 31;
+    NSUInteger result = [super hash];
+
+    result = prime * result + self.lastConnection.hash;
+    result = prime * result + self.locale.hash;
+
+    result = prime * result + self.batteryHealth;
+    result = prime * result + self.coAlarmState;
+    result = prime * result + self.smokeAlarmState;
+
+    result = prime * result + (self.isManualTestActive ? intValueForYes : intValueForNo);
+
+    result = prime * result + self.lastManualTestTime.hash;
+
+    result = prime * result + self.uiColorState;
+
+    return result;
+}
+
+- (BOOL)isEqual:(id)other {
+    if (other == self)
+        return YES;
+
+    if (!other || ![[other class] isEqual:[self class]])
+        return NO;
+
+    if (![super isEqual:other])
+        return NO;
+
+    NestSDKSmokeCOAlarm *otherSmokeCOAlarm = (NestSDKSmokeCOAlarm *) other;
+    return (([NestSDKUtils object:self.lastConnection isEqualToObject:otherSmokeCOAlarm.lastConnection]) &&
+            ([NestSDKUtils object:self.locale isEqualToObject:otherSmokeCOAlarm.locale]) &&
+            (self.batteryHealth == otherSmokeCOAlarm.batteryHealth) &&
+            (self.coAlarmState == otherSmokeCOAlarm.coAlarmState) &&
+            (self.smokeAlarmState == otherSmokeCOAlarm.smokeAlarmState) &&
+            (self.isManualTestActive == otherSmokeCOAlarm.isManualTestActive) &&
+            ([NestSDKUtils object:self.lastManualTestTime isEqualToObject:otherSmokeCOAlarm.lastManualTestTime]) &&
+            (self.uiColorState == otherSmokeCOAlarm.uiColorState));
+}
 
 @end

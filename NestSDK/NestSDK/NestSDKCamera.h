e1e5213f84e007e2c1139221a666ef977fefc9b5
petroakzhygitov <petro.akzhygitov@gmail.com>
2016-01-14 12:21:42 +0200

    Camera data model refactored and cleaned up

--- NestSDK/NestSDK/NestSDKCamera.h
+++ NestSDK/NestSDK/NestSDKCamera.h
@@ -1,34 +1,71 @@
+// Copyright (c) 2016 Petro Akzhygitov <petro.akzhygitov@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 #import <Foundation/Foundation.h>
 #import <JSONModel/JSONModel.h>
 #import "NestSDKDevice.h"
 
 @class NestSDKCameraLastEvent;
 
-#pragma mark macros
-
-#pragma mark const
-
-#pragma mark enum
-
-#pragma mark typedef
-
-#pragma mark Protocol
-
 @interface NestSDKCamera : NestSDKDevice
 #pragma mark Properties
 
-@property(nonatomic) BOOL is_streaming;
-@property(nonatomic) BOOL is_audio_input_enabled;
+/**
+ * Camera status, either on and actively streaming video, or off.
+ *
+ * Learn how to set Nest Cam streaming based on Away mode: https://nest.com/ie/support/article/Can-I-have-my-Nest-Cam-turn-on-automatically-when-my-Nest-Thermostat-goes-into-Away-mode
+ */
+@property(nonatomic) BOOL isStreaming;
+/**
+ * Camera microphone status, either on and listening, or off.
+ *
+ * Learn more about Nest Cam audio setting: https://nest.com/support/article/Does-Nest-Cam-have-audio
+ */
+@property(nonatomic) BOOL isAaudioInputEnabled;
 
-@property(nonatomic, copy) NSString <Optional> *last_is_online_change;
+/**
+ * Timestamp that identifies the last change to the online status.
+ */
+@property(nonatomic) NSDate <Optional> *lastIsOnlineChange;
 
-@property(nonatomic) BOOL is_video_history_enabled;
+/**
+ * Nest Aware with Video History subscription status (subscription active or not).
+ *
+ * Learn more about Nest Aware with Video History: https://nest.com/support/article/What-do-I-get-with-Nest-Aware-for-Nest-Cam
+ */
+@property(nonatomic) BOOL isVideoHistoryEnabled;
 
-@property(nonatomic, copy) NSString <Optional> *web_url;
-@property(nonatomic, copy) NSString <Optional> *app_url;
+/**
+ * Web URL (deep link) to the live camera feed at home.nest.com.
+ */
+@property(nonatomic, copy) NSString <Optional> *webUrl;
 
-@property(nonatomic) NestSDKCameraLastEvent *last_event;
+/**
+ * App URL (deep link) to the live camera feed in the Nest app.
+ */
+@property(nonatomic, copy) NSString <Optional> *appUrl;
 
-#pragma mark Methods
+/**
+ * This object captures information about the last event that triggered a notification.
+ * In order to capture last event data, the Nest Cam must have a Nest Aware with Video History subscription.
+ */
+@property(nonatomic) NestSDKCameraLastEvent *lastEvent;
 
 @end

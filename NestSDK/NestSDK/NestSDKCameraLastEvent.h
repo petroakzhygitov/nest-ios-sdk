3f6c9d904bd16322b3f65ca3bf6e5e7064023781
petroakzhygitov <petro.akzhygitov@gmail.com>
2016-01-14 14:04:03 +0200

    Camera last event model data refactored and cleaned up

--- NestSDK/NestSDK/NestSDKCameraLastEvent.h
+++ NestSDK/NestSDK/NestSDKCameraLastEvent.h
@@ -1,35 +1,78 @@
+// Copyright (c) 2016 Petro Akzhygitov <petro.akzhygitov@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 #import <Foundation/Foundation.h>
 #import "JSONModel.h"
+#import "NestSDKDataModel.h"
 
-#pragma mark macros
-
-#pragma mark const
-
-#pragma mark enum
-
-#pragma mark typedef
-
-#pragma mark Protocol
-
-@protocol NestSDKCameraLastEvent
-
-@end
-
-
-@interface NestSDKCameraLastEvent : JSONModel
+/**
+ * Camera last event data model.
+ *
+ * This object captures information about the last event that triggered a notification.
+ * In order to capture last event data, the Nest Cam must have a Nest Aware with Video History subscription.
+ *
+ * Learn more about Nest Aware with Video History subscription: https://nest.com/support/article/What-do-I-get-with-Nest-Aware-for-Nest-Cam
+ */
+@interface NestSDKCameraLastEvent : NestSDKDataModel
 #pragma mark Properties
 
-@property(nonatomic) BOOL has_sound;
-@property(nonatomic) BOOL has_motion;
+/**
+ * Sound event - sound was detected.
+ */
+@property(nonatomic) BOOL hasSound;
 
-@property(nonatomic, copy) NSString <Optional> *start_time;
-@property(nonatomic, copy) NSString <Optional> *end_time;
+/**
+ * Motion event - motion was detected.
+ */
+@property(nonatomic) BOOL hasMotion;
 
-@property(nonatomic, copy) NSString <Optional> *web_url;
-@property(nonatomic, copy) NSString <Optional> *app_url;
-@property(nonatomic, copy) NSString <Optional> *image_url;
-@property(nonatomic, copy) NSString <Optional> *animated_image_url;
+/**
+ * Event start time.
+ */
+@property(nonatomic) NSDate <Optional> *startTime;
 
-#pragma mark Methods
+/**
+ * Event end time.
+ */
+@property(nonatomic) NSDate <Optional> *endTime;
+
+/**
+ * Web URL (deep link) to the last sound or motion event at home.nest.com.
+ * Used to display the last recorded event, and requires user to be signed in to the account.
+ */
+@property(nonatomic, copy) NSString <Optional> *webUrl;
+
+/**
+ * Nest app URL (deep link) to the last sound or motion event.
+ * Used to display the last recorded event, and requires user to be signed in to the account.
+ */
+@property(nonatomic, copy) NSString <Optional> *appUrl;
+
+/**
+ * URL (link) to the image file captured for a sound or motion event.
+ */
+@property(nonatomic, copy) NSString <Optional> *imageUrl;
+
+/**
+ * URL (link) to the gif file captured for a sound or motion event.
+ */
+@property(nonatomic, copy) NSString <Optional> *animatedImageUrl;
 
 @end
--- NestSDK/NestSDK/NestSDKCameraLastEvent.m
+++ NestSDK/NestSDK/NestSDKCameraLastEvent.m
@@ -1,32 +1,81 @@
+// Copyright (c) 2016 Petro Akzhygitov <petro.akzhygitov@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 #import "NestSDKCameraLastEvent.h"
-
-#pragma mark macros
-
-#pragma mark const
-
-#pragma mark enum
-
-#pragma mark typedef
+#import "NestSDKUtils.h"
 
 
-@implementation NestSDKCameraLastEvent {
-#pragma mark Instance variables
-}
-
-#pragma mark Initializer
-
-#pragma mark Private
-
-#pragma mark Notification selectors
-
+@implementation NestSDKCameraLastEvent
 #pragma mark Override
 
-#pragma mark Public
+- (void)setStartTimeNSString:(NSString *)startTimeString {
+    self.startTime = [NestSDKUtils dateWithISO8601FormatDateString:startTimeString];
+}
 
-#pragma mark IBAction
+- (id)JSONObjectForStartTime {
+    return [NestSDKUtils iso8601FormatDateStringWithDate:self.startTime];
+}
 
-#pragma mark Protocol @protocol-name
+- (void)setEndTimeNSString:(NSString *)endTimeString {
+    self.endTime = [NestSDKUtils dateWithISO8601FormatDateString:endTimeString];
+}
 
-#pragma mark Delegate @delegate-name
+- (id)JSONObjectForEndTime {
+    return [NestSDKUtils iso8601FormatDateStringWithDate:self.endTime];
+}
 
+- (NSUInteger)hash {
+    NSUInteger intValueForYes = 1231;
+    NSUInteger intValueForNo = 1237;
+
+    NSUInteger prime = 31;
+    NSUInteger result = 1;
+
+    result = prime * result + (self.hasSound ? intValueForYes : intValueForNo);
+    result = prime * result + (self.hasMotion ? intValueForYes : intValueForNo);
+
+    result = prime * result + self.startTime.hash;
+    result = prime * result + self.endTime.hash;
+    result = prime * result + self.webUrl.hash;
+    result = prime * result + self.appUrl.hash;
+    result = prime * result + self.imageUrl.hash;
+    result = prime * result + self.animatedImageUrl.hash;
+
+    return result;
+}
+
+- (BOOL)isEqual:(id)other {
+    if (other == self)
+        return YES;
+
+    if (!other || ![[other class] isEqual:[self class]])
+        return NO;
+
+    NestSDKCameraLastEvent *otherCamera = (NestSDKCameraLastEvent *) other;
+    return ((self.hasSound == otherCamera.hasSound) &&
+            (self.hasMotion == otherCamera.hasMotion) &&
+            ([NestSDKUtils object:self.startTime isEqualToObject:otherCamera.startTime]) &&
+            ([NestSDKUtils object:self.endTime isEqualToObject:otherCamera.endTime]) &&
+            ([NestSDKUtils object:self.webUrl isEqualToObject:otherCamera.webUrl]) &&
+            ([NestSDKUtils object:self.appUrl isEqualToObject:otherCamera.appUrl]) &&
+            ([NestSDKUtils object:self.imageUrl isEqualToObject:otherCamera.imageUrl]) &&
+            ([NestSDKUtils object:self.animatedImageUrl isEqualToObject:otherCamera.animatedImageUrl]));
+}
 @end

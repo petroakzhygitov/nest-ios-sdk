891287796c55eed23d6ed26cc39ef8b4e10c6209
petroakzhygitov <petro.akzhygitov@gmail.com>
2016-01-16 14:41:17 +0200

    Code cleanup

--- NestSDK/NestSDK/NestSDKError.m
+++ NestSDK/NestSDK/NestSDKError.m
@@ -1,6 +1,24 @@
-#import "NestSDKError.h"
+// Copyright (c) 2016 Petro Akzhygitov <petro.akzhygitov@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
-#pragma mark macros
+#import "NestSDKError.h"
 
 #pragma mark const
 
@@ -19,24 +37,8 @@ static NSString *const kErrorFailureReasonStingUnexpectedType = @"Argument '%@' 
 
 static NSString *const kErrorRecoverySuggestionStingMainBundleKeyRequired = @"Check '%@' key is set in your info.plist file.";
 
-#pragma mark enum
 
-#pragma mark typedef
-
-
-@implementation NestSDKError {
-#pragma mark Instance variables
-}
-
-
-#pragma mark Initializer
-
-#pragma mark Private
-
-#pragma mark Notification selectors
-
-#pragma mark Override
-
+@implementation NestSDKError
 #pragma mark Public
 
 + (NSString *)errorDomain {
@@ -99,11 +101,5 @@ static NSString *const kErrorRecoverySuggestionStingMainBundleKeyRequired = @"Ch
             recoverySuggestion:nil
                underlyingError:error];
 }
-
-#pragma mark IBAction
-
-#pragma mark Protocol @protocol-name
-
-#pragma mark Delegate @delegate-name
 
 @end

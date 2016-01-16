ef3c600efb9b99c7f4f8bb4a34485004e3d40bb6
petroakzhygitov <petro.akzhygitov@gmail.com>
2016-01-16 14:28:46 +0200

    Added human readable error descriptions

--- NestSDK/NestSDK/NestSDKError.m
+++ NestSDK/NestSDK/NestSDKError.m
@@ -6,6 +6,19 @@
 
 NSString *const NestSDKErrorDomain = @"NestSDKErrorDomain";
 
+static NSString *const kErrorDescriptionStingArgumentRequired = @"Method argument required error!";
+static NSString *const kErrorDescriptionStingMainBundleKeyRequired = @"Main bundle key is required error!";
+static NSString *const kErrorDescriptionStingBadResponse = @"Bad response received error!";
+static NSString *const kErrorDescriptionStingUnexpectedType = @"Unexpected type of argument error!";
+static NSString *const kErrorDescriptionStingUnableToParseData = @"Unable to parse data error!";
+
+static NSString *const kErrorFailureReasonStingArgumentRequired = @"Value for '%@' is required.";
+static NSString *const kErrorFailureReasonStingMainBundleKeyRequired = @"'%@' key in main bundle is required.";
+static NSString *const kErrorFailureReasonStingBadResponse = @"Received wrong status code: %d";
+static NSString *const kErrorFailureReasonStingUnexpectedType = @"Argument '%@' has unexpected type.";
+
+static NSString *const kErrorRecoverySuggestionStingMainBundleKeyRequired = @"Check '%@' key is set in your info.plist file.";
+
 #pragma mark enum
 
 #pragma mark typedef
@@ -31,62 +44,59 @@ NSString *const NestSDKErrorDomain = @"NestSDKErrorDomain";
 }
 
 + (NSError *)errorWithCode:(NSInteger)code
-                  userInfo:(NSDictionary *)userInfo
-                   message:(NSString *)message
+               description:(NSString *)description
+             failureReason:(NSString *)failureReason
+        recoverySuggestion:(NSString *)recoverySuggestion
            underlyingError:(NSError *)underlyingError {
+
+    NSMutableDictionary *userInfo = [[NSMutableDictionary alloc] init];
+    userInfo[NSLocalizedDescriptionKey] = description;
+    userInfo[NSLocalizedFailureReasonErrorKey] = failureReason;
+    userInfo[NSLocalizedRecoverySuggestionErrorKey] = recoverySuggestion;
+    userInfo[NSUnderlyingErrorKey] = underlyingError;
 
     return [[NSError alloc] initWithDomain:[self errorDomain] code:code userInfo:userInfo];
 }
 
 + (NSError *)argumentRequiredErrorWithName:(NSString *)name message:(NSString *)message {
-    if (!message) {
-        message = [[NSString alloc] initWithFormat:@"Value for %@ is required.", name];
-    }
-
     return [self errorWithCode:NestSDKErrorCodeArgumentRequired
-                      userInfo:nil
-                       message:message
+                   description:kErrorDescriptionStingArgumentRequired
+                 failureReason:[NSString stringWithFormat:kErrorFailureReasonStingArgumentRequired, name]
+            recoverySuggestion:nil
                underlyingError:nil];
 }
 
 
 + (NSError *)mainBundleKeyRequiredErrorWithKey:(NSString *)key message:(NSString *)message {
-    if (!message) {
-        message = [[NSString alloc] initWithFormat:@"%@ key in main bundle is required. Check it is set in your info.plist file.", key];
-    }
-
     return [self errorWithCode:NestSDKErrorCodeMainBundleKeyRequired
-                      userInfo:nil
-                       message:message
+                   description:kErrorDescriptionStingMainBundleKeyRequired
+                 failureReason:[NSString stringWithFormat:kErrorFailureReasonStingMainBundleKeyRequired, key]
+            recoverySuggestion:[NSString stringWithFormat:kErrorRecoverySuggestionStingMainBundleKeyRequired, key]
                underlyingError:nil];
 }
 
 + (NSError *)badResponseErrorWithStatusCode:(NSInteger)statusCode message:(NSString *)message {
-    if (!message) {
-        message = [[NSString alloc] initWithFormat:@"Received wrong status code: %d", statusCode];
-    }
-
     return [self errorWithCode:NestSDKErrorCodeBadResponse
-                      userInfo:nil
-                       message:message
+                   description:kErrorDescriptionStingBadResponse
+                 failureReason:[NSString stringWithFormat:kErrorFailureReasonStingBadResponse, statusCode]
+            recoverySuggestion:nil
                underlyingError:nil];
 }
 
 + (NSError *)unexpectedArgumentTypeErrorWithName:(NSString *)name message:(NSString *)message {
-    if (!message) {
-        message = [[NSString alloc] initWithFormat:@"Argument %@ has unexpected type", name];
-    }
 
     return [self errorWithCode:NestSDKErrorCodeUnexpectedArgumentType
-                      userInfo:nil
-                       message:message
+                   description:kErrorDescriptionStingUnexpectedType
+                 failureReason:[NSString stringWithFormat:kErrorFailureReasonStingUnexpectedType, name]
+            recoverySuggestion:nil
                underlyingError:nil];
 }
 
 + (NSError *)unableToParseDataErrorWithUnderlyingError:(NSError *)error {
-    return [self errorWithCode:NestSDKErrorCodeUnexpectedArgumentType
-                      userInfo:nil
-                       message:nil
+    return [self errorWithCode:NestSDKErrorCodeUnableToParseData
+                   description:kErrorDescriptionStingUnableToParseData
+                 failureReason:nil
+            recoverySuggestion:nil
                underlyingError:error];
 }
 

7f3c12ea93fabfa6891140582a727a514c79430f
petroakzhygitov <petro.akzhygitov@gmail.com>
2016-01-23 19:49:33 +0200

    AccessToken cache clean up

--- NestSDK/NestSDK/NestSDKAccessTokenCache.m
+++ NestSDK/NestSDK/NestSDKAccessTokenCache.m
@@ -1,31 +1,34 @@
+// Copyright (c) 2016 Petro Akzhygitov <petro.akzhygitov@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 #import <SSKeychain/SSKeychain.h>
 #import "NestSDKAccessTokenCache.h"
 #import "NestSDKAccessToken.h"
-
-#pragma mark macros
 
 #pragma mark const
 
 static NSString *const kServiceName = @"NestSDKApplicationDelegate";
 static NSString *const kAccountName = @"default";
 
-#pragma mark enum
 
-#pragma mark typedef
-
-
-@implementation NestSDKAccessTokenCache {
-#pragma mark Instance variables
-}
-
-#pragma mark Initializer
-
-#pragma mark Private
-
-#pragma mark Notification selectors
-
-#pragma mark Override
-
+@implementation NestSDKAccessTokenCache
 #pragma mark Public
 
 - (NestSDKAccessToken *)fetchAccessToken {
@@ -49,11 +52,5 @@ static NSString *const kAccountName = @"default";
 - (void)clearCache {
     [SSKeychain setPasswordData:[NSData data] forService:kServiceName account:kAccountName];
 }
-
-#pragma mark IBAction
-
-#pragma mark Protocol @protocol-name
-
-#pragma mark Delegate @delegate-name
 
 @end
